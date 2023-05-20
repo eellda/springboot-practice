9c73d216ff0eb2ee1613dc6668a4abf362c2f16f
eellda <tree500253@gmail.com>
2023-05-20 20:32:12 +0900

    Swagger definition setting

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,26 +14,33 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "spring Boot API Test",
+                description = "API 명세서",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "sm",
+                        email = "test@test.co.kr"
+                )
+        )
+)
+
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
 
     // default 구조
     @Bean
-    public Docket api() {
+    public Docket swaggerAPI(){
+        //Docket : swagger Bean
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(true) //기본 응답 메시지 표시 여부
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo")) // 스캔 범위 지정
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo")) //swagger탐색 대상 패키지
                 .paths(PathSelectors.any())
                 .build();
-    }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot API Test with Swagger")
-                .description("practice")
-                .version("1.0.0")
-                .build();
     }
 }

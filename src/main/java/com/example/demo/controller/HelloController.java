9486e5b8cefc7bb028690d104aa634788dbc3a71
eellda <tree500253@gmail.com>
2023-05-21 22:14:27 +0900

    logback setting

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @RequestMapping("hello")
     public String Hello() {
         return "hello world!";
+    }
+
+    @PostMapping("/log-test")
+    public void logTest() {
+        LOGGER.trace("trace");
+        LOGGER.debug("debug");
+        LOGGER.info("info");
+        LOGGER.warn("warn");
+        LOGGER.error("error");
     }
 }
--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.data.dto.ProductDto;
 import com.example.demo.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/product")
 public class ProductController {
     private ProductService productService;
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
     public ProductController(ProductService productService) {
@@ -17,7 +20,15 @@ public class ProductController {
 
     @GetMapping("/{productId}")
     public ProductDto getProduct(@PathVariable String productId) {
-        return productService.getProduct(productId);
+
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[ProductController] perform {} of Demo API", "getProduct");
+        ProductDto productDto = productService.getProduct(productId);
+
+        LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms",
+                productDto.getProductId(), productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis() - startTime));
+        // {}를 사용함으로써 값을 각각 할당 가능
+        return productDto;
     }
 
     @PostMapping()

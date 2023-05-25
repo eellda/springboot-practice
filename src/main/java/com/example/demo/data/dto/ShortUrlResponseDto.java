3a7d34ffa0c85271de93286c4e38b273232216dc
eellda <tree500253@gmail.com>
2023-05-25 11:56:19 +0900

    Redis test SNAPSHOT

--- src/main/java/com/example/demo/data/dto/ShortUrlResponseDto.java
+++ src/main/java/com/example/demo/data/dto/ShortUrlResponseDto.java
@@ -1,13 +1,17 @@
 package com.example.demo.data.dto;
 
+import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Builder
+@RedisHash(value = "shortUrl", timeToLive = 60)
 public class ShortUrlResponseDto {
+    @Id
     private String orgUrl;
     private String shortUrl;
 }
--- src/main/java/com/example/demo/data/repository/ShortUrlRepository.java
+++ src/main/java/com/example/demo/data/repository/ShortUrlRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.data.repository;
 
+import com.example.demo.data.dto.ShortUrlResponseDto;
 import com.example.demo.data.entity.ShortUrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ShortUrlRepository extends JpaRepository<ShortUrlEntity, Long> {
-    ShortUrlEntity findByUrl(String url);
-    ShortUrlEntity findByOrgUrl(String originalUrl);
+public interface ShortUrlRepository extends CrudRepository<ShortUrlResponseDto, String> {
+
 }
